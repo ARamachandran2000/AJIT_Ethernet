24e5aa6de399e552c1f6eb1cbb85bf74875629fe
Harshad Ugale <86884459+HarshadUgale5@users.noreply.github.com>
2021-10-27 10:13:37 +0530

    Added basic version of parser.
    There are some errors in algorithm will be sorted out shortly.

---
+++ NIC/rx_from_mac/build.sh
@@ -0,0 +1,8 @@
+CWD=$(pwd)
+cd hw
+. compile.sh
+cd $CWD
+. build_aa2c_tb.sh
+#cd $CWD
+#. build_ghdl_tb.sh
+#. build_ghdl_model.sh
---
+++ NIC/rx_from_mac/testbench/test_compile.sh
@@ -0,0 +1,2 @@
+# only to check if the tb compiles ok.
+gcc -o tb_compile_test_only  -I $AHIR_RELEASE/include testbench.c -I ../src -I ../headers -L $AHIR_RELEASE/lib -lPipeHandler -lBitVectors -lpthread -DAA2C

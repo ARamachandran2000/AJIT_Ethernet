a69211cf5764511a661c0550734218f364f85ed0
ARamachandran2000 <akshatramachandran@gmail.com>
2022-02-11 12:16:22 +0530

    Added Packet Functions

--- code/NicWithMacIf/Nic/PacketFormat.h
+++ code/NicWithMacIf/Nic/PacketFormat.h
@@ -1,6 +1,7 @@
 #ifndef PacketFormat_h___
 #define PacketFormat_h___
 
+#define HEADER_BYTE_MASK 0x00_00_00_00_FF_FF_FF_FF_FF_FF_FF_FF_FF_FF_FF_FF;
 typedef struct NicPacket__ {
 	// dwords [0] is the control dword.
 	// what is the format?
@@ -17,19 +18,90 @@ typedef struct NicPacket__ {
 	uint64_t dwords[256];
 } NicPacket;
 
+
+
+uint64_t getSliceFromWord (uint64_t word, uint8_t high, uint8_t low)
+{
+
+	word = word << (63 - high); // First remove extra bits in MSB 
+	word = word >> ((63 - high) + low); // Then remove extra bits in LSB
+	return word;
+
+
+}
+
+
+uint64_t setSliceOfWord (uint64_t word, uint8_t high, uint8_t low, uint8_t wval)
+{
+
+	uint64_t word_mask = -1;
+	word_mask = word_mask >> (63 - high + low);
+	wval = wval & word_mask;
+	wval = wval << low;
+
+	word_mask = word_mask <<low;
+	word = word & (~word_mask);
+	word = word | value;
+	return word;
+
+}
+
 void getPacketControlInfo 
 	(NicPacket* np,
 		uint8_t *index_of_last_dword,
-		uint8_t *byte_mask_of_last_dword);
-void getPacketHeader (NicPacket* np, uint64_t* h0, uint64_t* h1);
-uint64_t getPacketDataDword (NicPacket* np, uint8_t index);
+		uint8_t *byte_mask_of_last_dword)
+
+
+{
+	
+	uint64_t control_info = np->dwords[0];
+	*byte_mask_of_last_dword = getSliceFromWord(control_info,7,0);
+	*index_of_last_dword = getSliceFromWord(control_info,15,8);
+
+}
+void getPacketHeader (NicPacket* np, uint64_t* h0, uint64_t* h1)
+{
+	uint64_t header_0 = np->dwords[1];
+	uint64_t header_1 = np->dwords[2];	
+	
+	*h0 = header_0;
+	*h1 = header_1 & (HEADER_BYTE_MASK);
+
+}
+
+uint64_t getPacketDataDword (NicPacket* np, uint8_t index)
+{
+
+	return np->dwords[index];
+
+}
+
 
 void setPacketControlInfo 
 	(NicPacket* np,
 		uint8_t index_of_last_dword,
-		uint8_t byte_mask_of_last_dword);
-void setPacketHeader (NicPacket* np, uint64_t h0, uint64_t h1);
-void setPacketDataDword (NicPacket* np, uint8_t index, uint64_t wdata);
+		uint8_t byte_mask_of_last_dword)
+{
+
+	uint64_t control_info = 0;
+	control_info = setSliceOfWord(control_info,7,0,byte_mask_of_last_dword);
+	control_info = setSliceOfWord(control_info,15,8,index_of_last_dword);
+	np -> dwords[0] = control_info;
+	
+}
+void setPacketHeader (NicPacket* np, uint64_t h0, uint64_t h1)
+{
+	
+	np->dwords[1] = h0;
+	np->dwords[2] = h1;
+
+}
+void setPacketDataDword (NicPacket* np, uint8_t index, uint64_t wdata)
+{
+
+	np->dwords[index]  = wdata;
+
+}
 
 
 #endif

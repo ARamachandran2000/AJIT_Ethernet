7cde151f0a28741b6bb1a16e1f2ce3fde5d0ce56
HarshadUgale5 <hbugaleofficial@gmail.com>
2022-02-22 15:12:52 +0530

    added  NIC to MAC flow part of testbench

--- code/NicWithMacIf/Nic/testbench/MAC.h
+++ code/NicWithMacIf/Nic/testbench/MAC.h
@@ -8,7 +8,10 @@
 #include<Pipes.h>
 
 #define MAX_PACKET_LENGTH_IN_BYTES	1500
-
+int __err_flag_ = 0;
+// lets have static address for now
+uint64_t destination_mac_address = 1;
+uint64_t source_mac_address = 2; 
 // This function genreates and transmits packet from MAC to NIC
 //	actual MAC sends	73 bits 	at a time.
 //			----------------------
@@ -22,17 +25,14 @@
 //
 //		2.mac_to_nic_data_0[63:0] = data[63:0]	-- 64 bit wide
 //	 	
-void MAC_To_NIC_data(void){
-
-	char pipe_to_send0[20], pipe_to_send_1[20];
+void MAC_To_NIC_data(void)
+{
+	char pipe_to_send0[20], pipe_to_send1[20];
 	sprintf(pipe_to_send0,"mac_to_nic_data_0");
 	sprintf(pipe_to_send1,"mac_to_nic_data_1");
 	
-	uint64_t data_64;
-	uint16_t data_16;
-	// lets have static address for now
-	uint64_t destination_mac_address = 1;
-	uint64_t source_mac_address = 2; 
+	uint64_t data_64 = 0;
+	uint16_t data_16 = 0;
 	
 	// fixed length packet
 	uint16_t length_in_bytes = 20 + 30; // 20 bytes is min ip header length
@@ -76,5 +76,75 @@ void MAC_To_NIC_data(void){
 		write_uint16(pipe_to_send1, data_16);		
 	}
 	// for last 64 bit word , tlast = 1;
-	// task remaing.
+	// clear all bits
+	data_64 = 0; data_16 = 0;
+	data_64 = (i << 8) | (0xff);
+	// tlast = 1
+	data_16 = (1 << 8); 
+	// write data to pipe
+	write_uint64(pipe_to_send0, data_64);
+	write_uint16(pipe_to_send1, data_16);		
+}
+
+// This function receives packet from NIC and checks it it's correct
+//	actual NIC sends	73 bits 	at a time.
+//			----------------------
+//			1bit	64bits	8bits
+//			tlast	tdata	tkeep	
+//	form C direct transfer of 73 bits to a pipe is not possible.
+//	since this function uses two pipes to receive that data.
+//	pipes :
+//		1.nic_to_mac_data_1[ 8:0] = data[72:64]	-- 16 bit wide
+//						(top 7 bits are unused)
+//
+//		2.nic_to_mac_data_0[63:0] = data[63:0]	-- 64 bit wide
+
+void NIC_to_MAC_data(void)
+{
+	char pipe_to_recv0[20], pipe_to_recv1[20];
+	sprintf(pipe_to_recv0,"nic_to_mac_data_0");
+	sprintf(pipe_to_recv1,"nic_to_mac_data_1");
+	
+	uint64_t data_64;
+	uint16_t data_16,length_in_bytes;
+	
+	data_64 = read_uint64(pipe_to_recv0);
+	data_16 = read_uint64(pipe_to_recv1);
+	
+	uint64_t received_src_mac_addr, received_dest_mac_adddr;
+	
+	// reconstruct received mac addresses.
+	received_dest_mac_addr = (data_64 >> 8) & 0x0000ffffffffffff;
+	received_src_mac_addr = ((data_64 >> 56) & 0x00000000000000ff) | ((data_16 << 8) & 0x000000000000ff00);
+	
+	// read pipes		
+	data_64 = read_uint64(pipe_to_recv0);
+	data_16 = read_uint64(pipe_to_recv1);
+	// update remaing bits of src mac addr.
+	received_src_mac_addr = (data_64 >> 8) & 0x00000000ffffffff;
+	if((received_src_mac_addres != source_mac_address) || (received_dest_mac_addr != destination_mac_address)){
+		__err_flag_ = 1; return;}
+
+	length_in_bytes = (data_64 >> 40) & 0x00000000000000ff;
+	
+	int i;
+	for(i = 0; i < (length_in_bytes - 8); i += 8)
+	{
+		data_64 = read_uint64(pipe_to_recv0);
+		data_16 = read_uint64(pipe_to_recv1);
+		if(((data_64 >> 8) & 0xff) != i){
+			fprintf(stderr,"\nData Missmatch Expected = %d, Received = %d\n", i,data_64);
+			__err_flag_ = 1;
+			break;
+		}
+	}
+	// read last word
+	data_64 = read_uint64(pipe_to_recv0);
+	data_16 = read_uint64(pipe_to_recv1);
+	if(((data_64 >> 8) & 0xff) != i)
+	{	
+		fprintf(stderr,"\nData Missmatch Expected = %d, Received = %d\n", i,data_64);
+		__err_flag_ = 1;
+		return;
+	}
 }

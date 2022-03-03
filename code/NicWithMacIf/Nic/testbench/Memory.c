0bd503e69f744c5b81afb5c7923064aaec1acbef
HarshadUgale5 <hbugaleofficial@gmail.com>
2022-03-03 12:02:58 +0530

    modified Memory.c

--- code/NicWithMacIf/Nic/testbench/Memory.c
+++ code/NicWithMacIf/Nic/testbench/Memory.c
@@ -42,6 +42,7 @@ int accessMemory(uint8_t requerster_id,
 			uint64_t* rdata);
 {
 	int __error_flg = 0;
+	// LOCK MUTEX
 	pthread_mutex_lock(&mutex_memory_lock);
 	// PERFORM MEMORY OPERATION
 	if(memory_lock_status[0] || memory_lock_status[1])
@@ -57,16 +58,21 @@ int accessMemory(uint8_t requerster_id,
 	else if(lock == 1) 
 		// lock the memory
 		memory_lock_status[requester_id] = 1;
+	// checek if there was error due memory lock
 	if(!__error_flg)
 	{
+		// no error due to memory locking
 		if(read_write_bar)
+			// read data
 			*(rdata) = memory_array[addr]; 
 		else 
-		{
+		{	
+			// read data so tha it will be modified based on byte_mask
 			uint64_t tmp_rdata = memory_array[addr];
 			int index = 0;
 			for(index = 0; index < 8; index++)
 			{
+				// modify data depending on byte_mask
 				if((byte_mask>>index) & 0x1)
 				{
 					// need to rethink a bit
@@ -77,13 +83,18 @@ int accessMemory(uint8_t requerster_id,
 					tmp_rdata = tmp_rdata;
 					
 			}
-			*(rdata) = tmp_rdata;
+			// write data
+			wdata = tmp_rdata;
+			memory_array[addr] = wdata;
 		}
 	}
+	// UNLOCK MUTEX
 	pthread_mutex_unlock(&mutex_memory_lock);
 	return(__error_flg);
 }
 
+
+// reads request from pipes(corresping to requester_id)
 void getReqFromTester(uint8_t requester_id,
 			uint8_t* lock,
 			uint8_t* rwbar,
@@ -97,6 +108,7 @@ void getReqFromTester(uint8_t requester_id,
 	sprintf(req_pipe1,"mem_req%d_pipe1",(int)requester_id);//64 bit wide
 	
 	uint64_t req1;
+	// read pipes
 	*(wdata) = read_uint64(req_pipe0);
 	req1 = read_uint64(req_pipe1);
 	*(lock)  = (req1 >> 45) & 0x01; 
@@ -105,6 +117,7 @@ void getReqFromTester(uint8_t requester_id,
 	*(bmask) = (req1 >> 36) & 0xff;
 }
 
+// sends responce to pipes(corresping to requester_id)
 void sendResponceToTester(uint8_t requester_id, int error, uint64_t rdata)
 {	
 	// pipes
@@ -115,6 +128,8 @@ void sendResponceToTester(uint8_t requester_id, int error, uint64_t rdata)
 	write_uint64(resp_pipe0,rdata);
 	write_uint64(resp_pipe1,error);
 }
+
+// this function reads request, performs memory operation and then sends responce. 
 void memoryServiceModel(uint8_t requester_id);
 {
 	while(1)
@@ -128,24 +143,17 @@ void memoryServiceModel(uint8_t requester_id);
 	}
 }
 
-
+// this daemon will serve memory requests from nic.
 void nicMemoryServiceDaemon()
 {
 	uint8_t nic_id = 1;
 	memoryServiceModel(nic_id);
 }DEFINE_THREAD(nicMemoryServiceDaemon);
 
+// this daemon will serve memory requests from cpu.
 void cpuMemoryServiceDaemon()
 {
 	uint8_t cpu_id = 0;
 	memoryServiceModel(cpu_id);
 }DEFINE_THREAD(cpuMemoryServiceDaemon);
-/*
-		req0 = read_uint64(mem_req_pipe_0);
-		req1 = read_uint64(mem_req_pipe_1);
-		char lock  = (req1 >> 45) & 0x1; 
-		char rwbar = (req1 >> 44) & 0x1;
-		uint32_t addr = (req1 >> 4) & 0xfffffff;
-		uint8_t bmask = (req1 >> 36) & 0xff;
-*/
 

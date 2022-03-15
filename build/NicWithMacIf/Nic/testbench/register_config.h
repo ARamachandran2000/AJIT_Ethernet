39bc1980c4af479734a454c3f34130e89932c56a
HarshadUgale5 <hbugaleofficial@gmail.com>
2022-03-15 15:05:25 +0530

    cleaned

--- build/NicWithMacIf/Nic/testbench/register_config.h
+++
@@ -1,4 +0,0 @@
-
-uint64_t getSliceFromWord(uint64_t, uint8_t, uint8_t );
-uint32_t setSliceOfWord_32(uint32_t, uint8_t, uint8_t, uint32_t);
-uint64_t setSliceOfWord_64(uint64_t, uint8_t, uint8_t, uint64_t);

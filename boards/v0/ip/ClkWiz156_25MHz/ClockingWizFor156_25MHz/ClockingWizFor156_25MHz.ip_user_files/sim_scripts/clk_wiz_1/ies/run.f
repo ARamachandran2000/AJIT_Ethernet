17d59a725aac49c2f8ac55730d6c69843273f65f
ARamachandran2000 <akshatramachandran@gmail.com>
2022-04-13 15:11:59 +0530

    Added MAC Clock Wiz

---
+++ boards/v0/ip/ClkWiz156_25MHz/ClockingWizFor156_25MHz/ClockingWizFor156_25MHz.ip_user_files/sim_scripts/clk_wiz_1/ies/run.f
@@ -0,0 +1,14 @@
+-makelib ies/xil_defaultlib -sv \
+  "/opt/Xilinx/Vivado/2017.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies/xpm \
+  "/opt/Xilinx/Vivado/2017.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../../ClockingWizFor156_25MHz.srcs/sources_1/ip/clk_wiz_1/clk_wiz_1_clk_wiz.v" \
+  "../../../../ClockingWizFor156_25MHz.srcs/sources_1/ip/clk_wiz_1/clk_wiz_1.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  glbl.v
+-endlib
+

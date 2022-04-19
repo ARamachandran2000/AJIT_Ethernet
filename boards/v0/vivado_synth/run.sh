516270fa22b03d0b12d58cf6401fcbe5d8b8ffa1
HarshadUgale5 <hbugaleofficial@gmail.com>
2022-04-19 15:07:20 +0530

    modified bitgen.tcl and run.sh

--- boards/v0/vivado_synth/run.sh
+++ boards/v0/vivado_synth/run.sh
@@ -1,6 +1,5 @@
 # source /vivado/Vivado/2014.4/settings64.sh
 source ~/digitalbashrc
-#source /home/Xilinx_2017.1/Vivado/2017.1/settings64.sh
-#source ~/../../media/harshad/16b6077c-9eb9-4ff7-8b7f-7aa365b8f2a0/opt/xilinx1/Vivado/2017.1/settings64.sh
-source ~/my_xil_dir/Vivado/2017.1/settings64.sh
+source /home/Xilinx_2017.1/Vivado/2017.1/settings64.sh
+#source ~/my_xil_dir/Vivado/2017.1/settings64.sh
 vivado -mode batch -source bitgen.tcl
